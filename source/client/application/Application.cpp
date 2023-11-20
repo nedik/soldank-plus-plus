38d325fd1165f17fed6d79d825f5d72a69a5675c
nedik <kknedi@gmail.com>
2023-11-20 00:36:59 +0000

    Add GameNetworkingSockets to the client

--- source/client/application/Application.cpp
+++ source/client/application/Application.cpp
@@ -8,22 +8,51 @@
 
 #include "rendering/Scene.hpp"
 
+#include <steam/steamnetworkingsockets.h>
+#include <steam/isteamnetworkingutils.h>
+
 #include <GLFW/glfw3.h>
+
 #include <cmath>
 #include <iostream>
 #include <memory>
 #include <chrono>
 #include <thread>
+#include <format>
+#include <span>
 
 namespace Soldat::Application
 {
 std::unique_ptr<Window> window;
 std::unique_ptr<World> world;
 
+SteamNetworkingMicroseconds log_time_zero;
+
+void DebugOutput(ESteamNetworkingSocketsDebugOutputType output_type, const char* message)
+{
+    SteamNetworkingMicroseconds time = SteamNetworkingUtils()->GetLocalTimestamp() - log_time_zero;
+    std::cout << std::format("{} {}", (double)time * 1e-6, message) << std::endl;
+    fflush(stdout);
+    if (output_type == k_ESteamNetworkingSocketsDebugOutputType_Bug) {
+        exit(1);
+    }
+}
+
 void Init()
 {
     window = std::make_unique<Window>();
     world = std::make_unique<World>();
+
+    SteamDatagramErrMsg err_msg;
+    if (!GameNetworkingSockets_Init(nullptr, err_msg)) {
+        std::cout << "GameNetworkingSockets_Init failed. " << std::span(err_msg).data()
+                  << std::endl;
+    }
+
+    log_time_zero = SteamNetworkingUtils()->GetLocalTimestamp();
+
+    SteamNetworkingUtils()->SetDebugOutputFunction(k_ESteamNetworkingSocketsDebugOutputType_Msg,
+                                                   DebugOutput);
 }
 
 void UpdateMouseButton(int button, int action)
@@ -78,5 +107,14 @@ void Run()
 void Free()
 {
     window.reset(nullptr);
+
+    // Give connections time to finish up.  This is an application layer protocol
+    // here, it's not TCP.  Note that if you have an application and you need to be
+    // more sure about cleanup, you won't be able to do this.  You will need to send
+    // a message and then either wait for the peer to close the connection, or
+    // you can pool the connection to see if any reliable data is pending.
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+    GameNetworkingSockets_Kill();
 }
 } // namespace Soldat::Application
