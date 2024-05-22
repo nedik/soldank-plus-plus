067cdb539a63af749425003fa797c7d55c0dc768
nedik <kknedi@gmail.com>
2024-05-22 19:41:49 +0100

    Add files for all possible Animation States

---
+++ source/shared/core/animations/states/LegsStandAnimationState.hpp
@@ -0,0 +1,13 @@
+#ifndef __LEGS_STAND_ANIMATION_STATE_HPP__
+#define __LEGS_STAND_ANIMATION_STATE_HPP__
+
+#include "core/animations/AnimationState.hpp"
+
+namespace Soldank
+{
+class LegsStandAnimationState : public Soldank::AnimationState
+{
+};
+} // namespace Soldank
+
+#endif
