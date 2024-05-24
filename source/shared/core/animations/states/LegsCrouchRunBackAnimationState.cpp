5bae38b0447693aabb4f6c1707984952c32caf17
nedik <kknedi@gmail.com>
2024-05-24 17:50:01 +0100

    Add Legs Crouch Run Back Animation State

--- source/shared/core/animations/states/LegsCrouchRunBackAnimationState.cpp
+++ source/shared/core/animations/states/LegsCrouchRunBackAnimationState.cpp
@@ -1,7 +1,7 @@
-#include "core/animations/states/LegsCrouchRunAnimationState.hpp"
+#include "core/animations/states/LegsCrouchRunBackAnimationState.hpp"
 
 #include "core/animations/states/LegsCrouchAnimationState.hpp"
-#include "core/animations/states/LegsCrouchRunBackAnimationState.hpp"
+#include "core/animations/states/LegsCrouchRunAnimationState.hpp"
 #include "core/animations/states/LegsStandAnimationState.hpp"
 #include "core/animations/states/LegsFallAnimationState.hpp"
 #include "core/animations/states/LegsRunBackAnimationState.hpp"
@@ -15,14 +15,14 @@
 
 namespace Soldank
 {
-LegsCrouchRunAnimationState::LegsCrouchRunAnimationState(
+LegsCrouchRunBackAnimationState::LegsCrouchRunBackAnimationState(
   const AnimationDataManager& animation_data_manager)
-    : AnimationState(animation_data_manager.Get(AnimationType::CrouchRun))
+    : AnimationState(animation_data_manager.Get(AnimationType::CrouchRunBack))
     , animation_data_manager_(animation_data_manager)
 {
 }
 
-std::optional<std::shared_ptr<AnimationState>> LegsCrouchRunAnimationState::HandleInput(
+std::optional<std::shared_ptr<AnimationState>> LegsCrouchRunBackAnimationState::HandleInput(
   Soldier& soldier)
 {
     if (!soldier.control.down) {
@@ -43,30 +43,26 @@ std::optional<std::shared_ptr<AnimationState>> LegsCrouchRunAnimationState::Hand
         return std::make_shared<LegsCrouchAnimationState>(animation_data_manager_);
     }
 
-    if (soldier.control.right && soldier.direction == -1) {
-        return std::make_shared<LegsCrouchRunBackAnimationState>(animation_data_manager_);
+    if (soldier.control.right && soldier.direction == 1) {
+        return std::make_shared<LegsCrouchRunAnimationState>(animation_data_manager_);
     }
 
-    if (soldier.control.left && soldier.direction == 1) {
-        return std::make_shared<LegsCrouchRunBackAnimationState>(animation_data_manager_);
+    if (soldier.control.left && soldier.direction == -1) {
+        return std::make_shared<LegsCrouchRunAnimationState>(animation_data_manager_);
     }
 
     return std::nullopt;
 }
 
-void LegsCrouchRunAnimationState::Update(Soldier& soldier)
+void LegsCrouchRunBackAnimationState::Update(Soldier& soldier)
 {
     soldier.stance = PhysicsConstants::STANCE_CROUCH;
-    if (soldier.legs_animation.GetSpeed() > 2) {
-        soldier.particle.velocity_.x /= (float)soldier.legs_animation.GetSpeed();
-        soldier.particle.velocity_.y /= (float)soldier.legs_animation.GetSpeed();
-    }
 
     if (soldier.on_ground) {
-        if (soldier.control.right && soldier.direction == 1) {
+        if (soldier.control.right && soldier.direction == -1) {
             glm::vec2 particle_force = soldier.particle.GetForce();
             soldier.particle.SetForce({ PhysicsConstants::CROUCHRUNSPEED, particle_force.y });
-        } else if (soldier.control.left && soldier.direction == -1) {
+        } else if (soldier.control.left && soldier.direction == 1) {
             glm::vec2 particle_force = soldier.particle.GetForce();
             soldier.particle.SetForce({ -PhysicsConstants::CROUCHRUNSPEED, particle_force.y });
         }
---
+++ source/shared/core/animations/states/LegsCrouchRunBackAnimationState.hpp
@@ -0,0 +1,26 @@
+#ifndef __LEGS_CROUCH_RUN_BACK_ANIMATION_STATE_HPP__
+#define __LEGS_CROUCH_RUN_BACK_ANIMATION_STATE_HPP__
+
+#include "core/animations/AnimationState.hpp"
+
+#include "core/animations/AnimationData.hpp"
+
+namespace Soldank
+{
+struct Soldier;
+
+class LegsCrouchRunBackAnimationState final : public Soldank::AnimationState
+{
+public:
+    LegsCrouchRunBackAnimationState(const AnimationDataManager& animation_data_manager);
+    ~LegsCrouchRunBackAnimationState() override = default;
+
+    std::optional<std::shared_ptr<AnimationState>> HandleInput(Soldier& soldier) final;
+    void Update(Soldier& soldier) final;
+
+private:
+    const AnimationDataManager& animation_data_manager_;
+};
+} // namespace Soldank
+
+#endif
