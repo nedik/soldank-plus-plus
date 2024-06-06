000f1bcc5123ad77c1b20ce063949c87a38b17d8
nedik <kknedi@gmail.com>
2024-06-06 22:15:17 +0100

    Add items

---
+++ source/shared/core/types/ItemType.hpp
@@ -0,0 +1,40 @@
+#ifndef __ITEM_TYPE_HPP__
+#define __ITEM_TYPE_HPP__
+
+#include <cstdint>
+
+namespace Soldank
+{
+enum class ItemType : std::uint8_t
+{
+    AlphaFlag = 1,
+    BravoFlag,
+    PointmatchFlag,
+    USSOCOM,
+    DesertEagles,
+    MP5,
+    Ak74,
+    SteyrAUG,
+    Spas12,
+    Ruger77,
+    M79,
+    Barrett,
+    Minimi,
+    Minigun,
+    Bow,
+    MedicalKit,
+    GrenadeKit,
+    FlamerKit,
+    PredatorKit,
+    VestKit,
+    BerserkKit,
+    ClusterKit,
+    Parachute,
+    Knife,
+    Chainsaw,
+    LAW,
+    M2,
+};
+}
+
+#endif
